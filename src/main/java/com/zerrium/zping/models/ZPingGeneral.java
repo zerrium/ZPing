579197a867bd3c77f5460ed502817b68dd3a62c7
LBR23 <>
2021-12-16 19:19:49 +1100

    Added branch name to version.

--- src/main/java/com/zerrium/zping/models/ZPingGeneral.java
+++ src/main/java/com/zerrium/zping/models/ZPingGeneral.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.Logger;
 
 public class ZPingGeneral {
     public static final String MOD_NAME = "ZPing";
-    public static final String MOD_VERSION = "0.1.0-SNAPSHOT";
+    public static final String MOD_VERSION = "0.1.0-SNAPSHOT.render";
     public static final String MOD_ID = "zping";
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
     public static final Identifier PING_PACKET_ID = new Identifier("zping", "ping");
